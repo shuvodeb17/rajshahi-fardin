0dd9b5daf1e227d2371f112cdf7a13b3c4440d45
shuvodeb17 <shuvodeb17014@gmail.com>
2022-07-17 11:30:59 +0600

    code modify

--- js/post.js
+++ js/post.js
@@ -18,41 +18,36 @@ window.onscroll = () => {
 }
 // navbar end here
 
-
-
-// post start here
-var userMainPost1 = document.querySelector('.user-post-main-class1');
-var userMainPost2 = document.querySelector('.user-post-main-class2');
-var normalPostInput = document.getElementById('normal-post-input');
+// Post start here
 var post = document.getElementById('post');
-var mainPostSec = document.getElementById('main-post-sec');
-var navbarSec = document.querySelector('.navbar');
-mainPostSec.style.display = 'none';
-
-
-normalPostInput.addEventListener('click', function () {
-  mainPostSec.style.display = 'block';
-  normalPostInput.style.display = 'none';
+var userPostMainOne = document.getElementById('user-post-main-one');
+var userPostMainTwo = document.getElementById('user-post-main-two');
+var topNavbar = document.getElementById('navbar');
+var normalPost = document.getElementById('normal-post-input');
+var MainPostPage = document.getElementById('main-post-sec');
+MainPostPage.style.display = 'none';
+normalPost.addEventListener('click', function () {
+  post.classList.add('post-add');
+  MainPostPage.style.display = 'block';
   post.style.background = 'black';
-  navbarSec.style.display = 'none';
-  userMainPost1.style.display = 'none';
-  userMainPost2.style.display = 'none';
+  normalPost.style.display = 'none';
+  topNavbar.style.display = 'none';
+  userPostMainOne.style.display = 'none';
+  userPostMainTwo.style.display = 'none';
 
-});
+})
 
-
+// Close Button
+var postMainPage = document.getElementById('post');
 var closeBtn = document.getElementById('close-btn');
 closeBtn.addEventListener('click', function () {
-  mainPostSec.style.display = 'none';
-  normalPostInput.style.display = 'block';
-  normalPostInput.style.margin = '2% auto';
+  normalPost.style.display = 'block';
+  MainPostPage.style.display = 'none';
+  normalPost.classList.add('normal-input-auto');
+  userPostMainOne.style.display = 'block';
+  userPostMainTwo.style.display = 'block';
   post.style.background = '#0080FF';
-  navbarSec.style.display = 'block'
-  userMainPost1.style.display = 'block';
-  userMainPost2.style.display = 'block';
-
+  post.classList.add('post-main-page')
+  topNavbar.style.display = 'block';
 });
-
-// user post start here
-// user post end here
 // post end here
